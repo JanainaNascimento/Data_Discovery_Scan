2a26734fda92449ee55d53bf1bfdad8b78ec2afa
FilhodEmer <emerson_rodrigues_c@outlook.com>
2022-06-14 20:04:28 -0300

    Start

---
+++ Scan.py
@@ -0,0 +1,15 @@
+import re
+
+with open('Projeto TCC.txt', 'r', encoding='UTF-8') as file:
+    content = file.readlines()
+
+    #print(content)
+    
+    for line in content:
+        if re.search('de', line):
+            print(line, re.findall('de', line))
+
+'''find = re.compile(r'de')
+print(find.search(line))
+print(find.findall(line))
+print(find.sub('of', line))'''
