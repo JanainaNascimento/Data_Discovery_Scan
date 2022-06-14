aac07172a2ccf5f07dce8a01bde33346ef014c55
FilhodEmer <emerson_rodrigues_c@outlook.com>
2022-06-14 20:35:37 -0300

    Uncommented

--- Scan.py
+++ Scan.py
@@ -3,7 +3,7 @@ import re
 with open('Projeto TCC.txt', 'r', encoding='UTF-8') as file:
     content = file.readlines()
 
-    print(content) # The entire file
+    print(content) #The entire file
     
     for line in content:
         if re.search('de', line):
