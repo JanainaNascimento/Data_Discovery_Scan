50eac2f7b0186056947acc91105dad54ed04d3c9
Janaina <jaaah.nas@gmail.com>
2022-06-19 15:57:14 -0300

    teste

--- Scan.py
+++ Scan.py
@@ -9,6 +9,7 @@ with open('Projeto TCC.txt', 'r', encoding='UTF-8') as file:
         if re.search('de', line):
             print(line, re.findall('de', line))
 
+
 '''find = re.compile(r'de')
 print(find.search(line))
 print(find.findall(line))
